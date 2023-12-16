a921d862a3c0d8ad3acb9b40e1c5da7f65f43df3
AAwasHere <amirmoaz@gmail.com>
2023-12-16 19:12:55 +0330

    phase 2 added

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import socket
+from socket import getservbyport
 
 def is_host_online(host, port):
     try:
@@ -10,11 +11,52 @@ def is_host_online(host, port):
     except (socket.error, socket.timeout):
         return False
 
+def scan_ports(host, port_range):
+    start_port, end_port = map(int, port_range.split())
+    open_ports = []
+
+    for port in range(start_port, end_port + 1):
+        if is_port_open(host, port):
+            service_name = get_service_name(port)
+            open_ports.append((port, service_name))
+
+    return open_ports
+
+def is_port_open(host, port):
+    try:
+        with socket.create_connection((host, port), timeout=1) as sock:
+            return True
+    except (socket.error, socket.timeout):
+        return False
+
+def get_service_name(port):
+    try:
+        service_name = getservbyport(port)
+        return service_name
+    except (socket.error, socket.herror):
+        return "Unknown"
+
+
 if __name__ == "__main__":
     target_host = input("Enter the target host: ")
-    target_port = int(input("Enter a port number to check (e.g., 80 for HTTP): "))
+    input_port = input("Enter a port number (e.g., 80) / port range(e.g., 80 85) to check : ")
 
-    if is_host_online(target_host, target_port):
-        print(f"{target_host}:{target_port} is online.")
-    else:
-        print(f"{target_host}:{target_port} is offline.")
+    if any(char.isspace() for char in input_port): #part 2: scanning port
+        port_range = input_port
+        open_ports = scan_ports(target_host, port_range)
+
+        if open_ports:
+            print(f"{target_host} {port_range} has the following open ports:")
+            for port, service_name in open_ports:
+                print(f"{target_host} {port}: {service_name}")
+        else:
+            print(f"No open ports found on {target_host}.")
+
+    else: #part1: if host is online
+
+        target_port = int(input_port)
+        if is_host_online(target_host, target_port):
+            print(f"{target_host}:{target_port} is online.")
+        else:
+            print(f"{target_host}:{target_port} is offline.")
+
